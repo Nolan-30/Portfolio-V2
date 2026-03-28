36d285c415cff0477b6704d6cdbd5d337b1a97ea
Nolan-30 <nolan.dalmeida07@gmail.com>
2026-03-28 21:26:49 +0100

    debut de la structure de ma stack

--- src/assets/components/pages/Stack.jsx
+++ src/assets/components/pages/Stack.jsx
@@ -7,6 +7,20 @@ export default function Stack() {
   return (
     <div className="languages-container">
       <div className="language-card">
+        <span className="html-css">
+          <img src="public/images/html-css-geber.png" height={70} width={120} />
+        </span>
+        <h2>HTML / CSS</h2>
+
+        <div className="language-description">
+          <p>
+            Ma porte d'entrée dans le web. C'est en réalisant mon premier projet
+            perso, Penthouse que j'ai découvert une réelle passion pour le
+            frontend et le plaisir de donner vie à des interfaces soignées.
+          </p>
+        </div>
+      </div>
+      <div className="language-card">
         <img src="public/images/react.png" height={50} width={50} />
         <h2>React</h2>
 
@@ -19,18 +33,7 @@ export default function Stack() {
           </p>
         </div>
       </div>
-      <div className="language-card">
-        <h2>React</h2>
 
-        <div className="language-description">
-          <p>
-            Ma bibliothèque de prédilection pour créer des interfaces
-            utilisateur dynamiques et performantes. Je l'utilise pour concevoir
-            des composants réutilisables et gérer l'état complexe de mes
-            applications avec fluidité.
-          </p>
-        </div>
-      </div>
       <div className="language-card">
         <h2>React</h2>
 
