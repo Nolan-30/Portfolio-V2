0be20f41babc59323bec9148d52e426847c5b919
Nolan-30 <nolan.dalmeida07@gmail.com>
2026-03-29 17:26:39 +0200

    structure des technos maitrisées terminé

--- src/assets/components/pages/Stack.jsx
+++ src/assets/components/pages/Stack.jsx
@@ -1,51 +1,49 @@
 import { motion } from "framer-motion";
-import { div, h1 } from "motion/react-client";
-import GradientText from "../animations/GradientText";
 import "./css/Stack.css";
 
 export default function Stack() {
+  // donnes pr chaque carte
+  const technologies = [
+    {
+      name: "HTML / CSS",
+      description:
+        "Ma porte d'entrée dans le web. C'est en réalisant mon premier projet perso, Penthouse que j'ai découvert une réelle passion pour le frontend et le plaisir de donner vie à des interfaces soignées.",
+    },
+    {
+      name: "React",
+      description:
+        "Ma bibliothèque favorite pour créer des interfaces et animations fluides. Je l'utilise pour concevoir des composants réutilisables tout en excellant davantage en JavaScript.",
+    },
+    {
+      name: "JavaScript",
+      description:
+        "Le moteur de mes projets. C'est avec ce langage que j'ai appris à rendre mes interfaces vivantes et interactives, transformant des designs statiques en véritables expériences utilisateur.",
+    },
+    {
+      name: "SQL",
+      description:
+        "L'art de structurer la donnée. Je l'utilise pour concevoir des bases de données relationnelles robustes et optimiser la persistance des informations au cœur de mes applications.",
+    },
+    {
+      name: "PHP",
+      description:
+        "Ce langage m'a permis de comprendre la gestion côté serveur et de créer mes premières architectures web complètes et fonctionnelles.",
+    },
+  ];
+
   return (
     <div className="languages-container">
-      <div className="language-card">
-        <span className="html-css">
-          <img src="public/images/html-css-geber.png" height={70} width={120} />
-        </span>
-        <h2>HTML / CSS</h2>
+      {technologies.map((tech, index) => (
+        <div className="language-card" key={index}>
+          <span className={tech.className}></span>
 
-        <div className="language-description">
-          <p>
-            Ma porte d'entrée dans le web. C'est en réalisant mon premier projet
-            perso, Penthouse que j'ai découvert une réelle passion pour le
-            frontend et le plaisir de donner vie à des interfaces soignées.
-          </p>
+          <h2>{tech.name}</h2>
+
+          <div className="language-description">
+            <p>{tech.description}</p>
+          </div>
         </div>
-      </div>
-      <div className="language-card">
-        <img src="public/images/react.png" height={50} width={50} />
-        <h2>React</h2>
-
-        <div className="language-description">
-          <p>
-            Ma bibliothèque de prédilection pour créer des interfaces
-            utilisateur dynamiques et performantes. Je l'utilise pour concevoir
-            des composants réutilisables et gérer l'état complexe de mes
-            applications avec fluidité.
-          </p>
-        </div>
-      </div>
-
-      <div className="language-card">
-        <h2>React</h2>
-
-        <div className="language-description">
-          <p>
-            Ma bibliothèque de prédilection pour créer des interfaces
-            utilisateur dynamiques et performantes. Je l'utilise pour concevoir
-            des composants réutilisables et gérer l'état complexe de mes
-            applications avec fluidité.
-          </p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
